299ebe58b6f4f6f6c99c12a84e73af1af8071125
Ed de Almeida <edvaldoajunior@gmail.com>
2017-01-07 03:08:04 -0200

    Gem funtionality

--- lib/naics.rb
+++ lib/naics.rb
@@ -10,10 +10,24 @@ module NAICS
   def self.search(code)
   	@code = code
   	@doc = Nokogiri::HTML(result_page)
+  	result = Hash.new 
+  	result[:description] = description
+  	result[:explanation] = explanation
+  	result
   end
 
   def self.result_page
   	HTTParty.post(@search_url, :query => {:code => @code})
+  end
+
+  def self.description
+  	arr = @doc.css("h3[class='sixDigit']").text.split
+  	arr.shift
+  	(arr.length > 0) ? arr.join(' ') : nil 	
+  end
+
+  def self.explanation
+  	@doc.css("p[class='copy sixDigitCopy']").text.strip.split('Illustrative Examples:')[0]
   end
 
   def self.version_info
